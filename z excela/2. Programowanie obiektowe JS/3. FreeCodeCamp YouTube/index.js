0f6581c8f5358f61d7bab7adb7bb982068433ae3
Arek Szambelan <a.szambelan@op.pl>
2023-06-03 22:31:11 +0200

    Aktualizacja FreeCodeCamp

--- z excela/2. Programowanie obiektowe JS/3. FreeCodeCamp YouTube/index.js
+++ z excela/2. Programowanie obiektowe JS/3. FreeCodeCamp YouTube/index.js
@@ -1,3 +1,5 @@
+/// GETTER i SETTER
+
 class Square {
     constructor(width) {
         this.width = width;
@@ -29,3 +31,24 @@ console.log(kwadratA.pole)
 console.log(kwadratA.pole)
 console.log(kwadratA.pole)      //... 5 wywolanie.
 console.log(kwadratA.liczbaWywolanPola) // 5
+
+
+// STATIC METHODS
+
+class Square2 {
+    constructor(width) {
+        this.width = width;
+        this.height = width;
+    }
+
+    static równość(a, b) {
+        return a.width * a.height === b.width * b.height;
+    }
+}
+const sq1 = new Square2(4);
+const sq2 = new Square2(5);
+const sq3 = new Square2(4);
+//Metody statyczne wywolujemy nie na instancji tylko na classie!
+console.log(Square2.równość(sq1, sq2))// false, bo 16!=25
+console.log(Square2.równość(sq1, sq3))// true, bo 16=16
+//Powyzsze zamiast sq1 i 2 mozemy wstawiac liczby, wiec metody statyczne w klasach w ogole nie opotrzebuja instancji tej klasy aby dzialac.
